e7d3d87e0413d59e25655f03f2a7c30eeda305f3
BAGATHE <lolemadaly@gmail.com>
2025-02-12 12:36:52 +0300

    configuration properties

--- src/main/java/com/itu/coworking/CoworkingApplication.java
+++ src/main/java/com/itu/coworking/CoworkingApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoworkingApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CoworkingApplication.class, args);
 	}
