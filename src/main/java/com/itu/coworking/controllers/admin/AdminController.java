82f611d73c7c83a59273d0bdd4aa1fd937866c37
BAGATHE <lolemadaly@gmail.com>
2025-02-13 10:09:37 +0300

    login admin

---
+++ src/main/java/com/itu/coworking/controllers/admin/AdminController.java
@@ -0,0 +1,31 @@
+package com.itu.coworking.controllers.admin;
+
+import com.itu.coworking.model.Utilisateur;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+    @GetMapping("/admin/dashboard")
+    public String dashboardAdmin(HttpSession session, Model model) {
+        Utilisateur utilisateurConnecte = (Utilisateur) session.getAttribute("utilisateur");
+
+        if (utilisateurConnecte == null) {
+            //
+            return "redirect:/admin/login";
+        }
+
+        model.addAttribute("utilisateur", utilisateurConnecte);
+
+        return "Admin/dashboard";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/loginAdmin";
+    }
+
+}
